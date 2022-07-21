f0989376bbb92fa9158c67e6677f95375570d8f6
Shackila <mvd.shakila@gmail.com>
2022-07-21 12:35:19 +0430

    main page is done.

---
+++ src/app.js
@@ -0,0 +1,29 @@
+var slideIndex = 0;
+carousel();
+carousel2();
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+function carousel2() {
+  var i;
+  var x = document.getElementsByClassName("mySlides2");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel2, 2000); // Change image every 2 seconds
+}
